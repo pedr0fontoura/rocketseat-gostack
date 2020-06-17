0eec7bd5a385f351f43d11845e1284252f4ced32
Pedro Dias <pedrofontoura@id.uff.br>
2020-06-16 23:14:38 -0300

    feature: Add route to like a repository

--- src/app.js
+++ src/app.js
@@ -68,7 +68,17 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' });
+  }
+
+  repositories[repositoryIndex].likes += 1;
+
+  return response.status(200).json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
