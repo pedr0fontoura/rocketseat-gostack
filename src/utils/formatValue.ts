2726e393ee034eb51e416ef1e2a5ed90f10f948f
Pedro Dias <pedrofontoura@id.uff.br>
2020-06-30 14:48:01 -0300

    feat: show balance and list transactions

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,6 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRl' }).format(
+    value,
+  );
 
 export default formatValue;
