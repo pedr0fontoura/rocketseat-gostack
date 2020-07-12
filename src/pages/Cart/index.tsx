2ebb17a08f11992023d4ca53ec5775be23ea32b8
Pedro Dias <pedrofontoura@id.uff.br>
2020-07-12 01:35:26 -0300

    feat: add hability to list products, add to cart, increment and decrement product quantity

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,11 +39,11 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
